92647bdeec65db87f89640ea4976a9f873a0aedb
IvoBertoni89 <116576845+IvoBertoni89@users.noreply.github.com>
2023-07-06 10:05:34 -0300

    api
    intento de funcionamineto de api

--- TUP-LC2-CINE-APP_Template/js/common.js
+++ TUP-LC2-CINE-APP_Template/js/common.js
@@ -1,4 +1,9 @@
+const CargarPelis = async() => {
+    const llamada = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=987174eff71006f71276ecb352a7837b");
+    console.log(llamada);
+}
 
+CargarPelis ();
 
 
 
